a399b11b1ce59fd984fd709e9c917f96f79cd65b
SirDiogoo <diogo.h.88@gmail.com>
2021-04-25 02:21:53 -0700

    first commit

--- ex1.py
+++ ex1.py
@@ -1,9 +1,8 @@
-pessoas = int(input("Quantidade de pessoas: "))
-if pessoas < 5:
-    print("n baixo de pessoas")
-elif pessoas >= 5 and pessoas <= 10:
-    print("n médio de pessoas")
-elif pessoas >= 11 and pessoas <= 15:
-    print("n elevado de pessoas")
-else:
-    print("n extremo de pessoas")
+#!/usr/bin/python3
+
+import glob
+for files in glob.glob("*.py"):
+    print(files)
+    f = open('ex1.txt', 'a')
+    f.writelines(files + "\n")
+    f.close()
